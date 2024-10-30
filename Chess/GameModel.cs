396671e347fec94899a385b30b93c32305d0453b
lavenderweb <novakbrajovic@outlook.com>
2024-10-30 08:59:52 -0400

    Add IncrementLengthValidator, customize ParsingErrorMessages for InputNumber fields.

--- Chess/GameModel.cs
+++ Chess/GameModel.cs
@@ -9,6 +9,7 @@ namespace LavenderChessClock1.Chess
 
         public IncrementType Player1Increment { get; set; } = IncrementType.NoIncrement;
 
+        [IncrementLengthValidator]
         public int Player1IncrementLength { get; set; } = 0;
 
         public bool SameForBoth { get; set; } = true;
@@ -18,6 +19,7 @@ namespace LavenderChessClock1.Chess
 
         public IncrementType Player2Increment { get; set; } = IncrementType.NoIncrement;
 
+        [IncrementLengthValidator]
         public int Player2IncrementLength { get; set; } = 0;
     }
 }
---
+++ Validators/IncrementLengthValidator.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LavenderChessClock1.Validators
+{
+    public class IncrementLengthValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Value for \"increment length\" cannot be null.", new[] { validationContext.MemberName });
+            }
+
+            int valueInt = (int)value;
+
+            if (!int.TryParse(value.ToString(), out valueInt) || valueInt < 0)
+            {
+                return new ValidationResult("Value for \"increment length\" must be a positive integer, or 0.", new[] { validationContext.MemberName });
+            }
+            else if (valueInt > 7200)
+            {
+                return new ValidationResult("Value for \"increment length\" must be less than or equal to 7200 (2 hours). ", new[] { validationContext.MemberName });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
